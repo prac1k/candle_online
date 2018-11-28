a7c68380e2a0a0fe5efa60abeaa0f22b428dc58a
prac1k <i.prac1k@gmail.com>
2018-11-28 18:18:13 +0200

    added button and counter

--- candle_online/src/App.js
+++ candle_online/src/App.js
@@ -1,41 +1,63 @@
-import React, { Component } from 'react';
-import logo from './candle.png';
-import './App.css';
+import logo from "./candle.png";
+import React from 'react';
+import './styles/App.css';
+import './components/Button'
 
-class App extends Component {
+class Button extends React.Component {
+    handleClick = () => {
+        this.props.onClickFunction(this.props.incrementValue)
+    }
+
     render() {
         return (
-            < div
-        className = "App" >
-            < header
-        className = "App-header" >
-            < img
-        src = {logo}
-        className = "App-logo"
-        alt = "logo" / >
-            < p >
-            Candle
-        On - line. Lit it !
-        < /p>
-        < /header>
-        < /div>
-    );
-
-        return (
-            < div
-        className = "button" >;
-    <
-        Counter / >
-        < /div>
-    );
+            <button onClick={this.handleClick}>
+                +{this.props.incrementValue}
+            </button>
+        )
     }
 }
 
-class Counter extends Component {
+const Result = props => {
+    return (
+        <div>
+            {props.counter}
+        </div>
+    )
+}
+
+class App extends React.Component {
+    state = { counter: 0 }
+
+    incrementCounter = incrementValue => {
+        this.setState(prevstate => ({
+            counter: prevstate.counter + incrementValue
+        }))
+    }
 
     render() {
-        return <button>0</button>;
+        return (
+            <div
+                className="App">
+                <header
+                    className="App-header">
+                    <img
+                        src={ logo }
+                        className="App-logo"
+                        alt="logo"/>
+                    <p>
+                        Candle
+                        On - line. Lit it !
+                    </p>
+                </header>
+                <div className={Button}>
+                    <Button incrementValue={1} onClickFunction={this.incrementCounter} />
+                </div>
+                <div className="Result">
+                    <Result counter={this.state.counter} />
+            </div>
+            </div>
+        )
     }
 }
 
-export default App;
+export default App
